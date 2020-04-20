79cc7e336be75b29bfcf92610fb8af5b4a7ccd7c
Santiagotm921 <43409794+Santiagotm921@users.noreply.github.com>
2020-04-19 20:47:45 -0500

    Se ha agregado todos los productos a la lista. Internamente tienen todas sus caracteristicas, pero estas no se mostraran al usuario, sino cuando ya este de click en el producto. Se ha creado un rectangulo con contorno, para que este sirva como contenedor de la informacion del producto. El modo responsive hasta el momento va bien. Ademas, se han agregado botones de colores para los productos.

--- public/scripts/listProducts.js
+++ public/scripts/listProducts.js
@@ -10,10 +10,10 @@ var products = [
     },
 
     {
-        name: 'Crazy BYW 2.0',
+        name: 'CRAZY BYW 2.0 PHARRELL WILLIAMS SHOES',
         price: 90,
         rating: 5,
-        type: 'Boots',
+        type: 'Athletics',
         gender: 'Men',
         color: 'yellow',
         img: '/images/Athletics/Crazy_BYW_2.0_Pharrell_Williams_Shoes_Yellow_FU7369_01_standard.png'
@@ -21,34 +21,185 @@ var products = [
     },
 
     {
-        name: 'Nite Jogger Shoes',
+        name: 'NITE JOGGER SHOES',
         price: 50,
         rating: 3,
-        type: 'Boots',
+        type: 'Athletics',
         gender: 'Women',
         color: 'white',
         img: '/images/Athletics/Nite_Jogger_Shoes_White_DA8666_01_standard.png'
     },
 
     {
-        name: 'NMD R1 Shoes Orange',
+        name: 'NMD_R1 SHOES',
         price: 70,
         rating: 4,
-        type: 'Sport shoe',
+        type: 'Athletics',
         gender: 'Men',
         color: 'orange',
         img: '/images/Athletics/NMD_R1_Shoes_Orange_EF4267_01_standard.png'
     },
 
     {
-        name: 'Ultraboost PB SHOES',
+        name: 'ULTRABOOST PB SHOES',
         price: 130,
         rating: 5,
-        type: 'Sport shoe',
+        type: 'Athletics',
         gender: 'Men',
         color: 'white',
         img: '/images/Athletics/Ultraboost PB SHOES.png'
-    }
+    },
+
+    {
+        name: 'ULTRABOOST 20 SHOES',
+        price: 90,
+        rating: 4.7,
+        type: 'Athletics',
+        gender: 'Women',
+        color: 'brown',
+        img: '/images/Athletics/Ultraboost_20_Shoes_Pink_EG0725_01_standard.png'
+    },
+
+    {
+        name: 'BAARA BOOTS',
+        price: 190,
+        rating: 4,
+        type: 'Boots',
+        gender: 'Men',
+        color: 'brown',
+        img: '/images/Boots/Baara_Boots_Brown_EE5531_01_standard.png'
+    },
+
+    {
+        name: 'SUPERSTAR ADV BOOTS',
+        price: 240,
+        rating: 3.8,
+        type: 'Boots',
+        gender: 'Men',
+        color: 'white',
+        img: '/images/Boots/Superstar_ADV_Boots_White_D97886_01_standard.png'
+    },
+
+    {
+        name: 'TERREX FREE',
+        price: 130,
+        rating: 4,
+        type: 'Sport shoe',
+        gender: 'Women',
+        color: 'brown',
+        img: '/images/Boots/Terrex_Free_Hiker_Hiking_Shoes_Brown_G28416_01_standard.png'
+    },
+
+    {
+        name: 'ADIZERO 8.0',
+        price: 95,
+        rating: 4.2,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'white',
+        img: '/images/Cleats/Adizero_8.0_Cleats_White_EE7452_01_standard.png'
+    },
+
+    {
+        name: 'ADIZERO SK CLEATS',
+        price: 95,
+        rating: 4.5,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'black',
+        img: '/images/Cleats/Adizero_8.0_SK_Cleats_Black_D97643_01_standard.png'
+    },
+
+    {
+        name: 'FREACK CARBON',
+        price: 85,
+        rating: 3.9,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'black',
+        img: '/images/Cleats/Freak_Carbon_Low_Cleats_Black_EE7202_01_standard.png'
+    },
+
+    {
+        name: 'PREDATOR ARCHIVE',
+        price: 100,
+        rating: 4.7,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'blue',
+        img: '/images/Cleats/Predator_Archive_Shoes_Black_EH2942_01_standard.png'
+    },
+
+    {
+        name: 'PREDATOR MUTATOR',
+        price: 100,
+        rating: 4.9,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'blue',
+        img: '/images/Cleats/Predator_Mutator_20+_Firm_Ground_Cleats_Blue_EG1512_01_standard.png'
+    },
+
+    {
+        name: 'X19 FRIMGROUND',
+        price: 120,
+        rating: 5,
+        type: 'Cleats',
+        gender: 'Men',
+        color: 'blue',
+        img: '/images/Cleats/X_19+_Firm_Ground_Cleats_Blue_EG7137_01_standard.png'
+    },
+
+    {
+        name: 'AMERICA HI SHOES',
+        price: 75,
+        rating: 4,
+        type: 'Hightops',
+        gender: 'Men',
+        color: 'white',
+        img: '/images/Hightops/Americana_Hi_Shoes_White_EF2803_01_standard.png'
+    },
+
+    {
+        name: 'BASKET PROFI',
+        price: 85,
+        rating: 4.3,
+        type: 'Hightops',
+        gender: 'Women',
+        color: 'orange',
+        img: '/images/Hightops/Basket_Profi_Shoes_Yellow_FW3103_01_standard.png'
+    },
+
+    {
+        name: 'ENTRAP MID',
+        price: 80,
+        rating: 4.1,
+        type: 'Hightops',
+        gender: 'Men',
+        color: 'white',
+        img: '/images/Hightops/Entrap_Mid_Shoes_White_EG4310_01_standard.png'
+    },
+
+    {
+        name: 'PRO MODEL2G',
+        price: 100,
+        rating: 4.3,
+        type: 'Hightops',
+        gender: 'Men',
+        color: 'blue',
+        img: '/images/Hightops/Pro_Model_2G_Shoes_Blue_EF9820_01_standard.png'
+    },
+
+    {
+        name: 'TOP TEN HISHOE',
+        price: 100,
+        rating: 4.3,
+        type: 'Hightops',
+        gender: 'Men',
+        color: 'red',
+        img: '/images/Hightops/Top_Ten_Hi_Shoes_Red_EF6368_01_standard.png'
+    },
+
 ];
 
 module.exports = products;
